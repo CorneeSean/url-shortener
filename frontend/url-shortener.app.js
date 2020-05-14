7b3bd798f6d9b5e3d9f6b9a7669760a04a7365e0
CorneeSean <radkuc@wp.pl>
2020-05-14 23:21:03 +0200

    feat(frontend): Bootstrap React App

--- frontend/url-shortener.app.js
+++
@@ -1,6 +0,0 @@
-<!--TODO: Replace the code below with proper React app bootstrap-->
-document.getElementById('app-root').innerHTML = `<button id="toggle">Toggle Widget</button>`;
-document.getElementById( 'toggle' ).addEventListener( 'click', () => {
-    window.parent.postMessage('url-shortener:toggle', '*');
-    document.body.classList.toggle('opened');
-} );
