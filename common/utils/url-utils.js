f9127173ca0fbe19dbd6bbd50f2658aed8eaaaf8
CorneeSean <radkuc@wp.pl>
2020-05-17 18:54:58 +0200

    fix(server): Remove addProtocol on get by hash and add logging

--- common/utils/url-utils.js
+++ common/utils/url-utils.js
@@ -1,4 +1,3 @@
-const PROTOCOL_REGEX = /^(http|https):\/\//;
 
 class UrlUtils {
     static isValidURL( url ) {
@@ -8,10 +7,6 @@ class UrlUtils {
         } catch (e) {
             return false;
         }
-    }
-
-    static addProtocol( url ) {
-        return  PROTOCOL_REGEX.test( url ) ? url : 'https://' + url;
     }
 }
 
