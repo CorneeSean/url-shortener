26426c1940ad4b52b05851bf4774163cfb8467ec
CorneeSean <radkuc@wp.pl>
2020-05-17 17:35:00 +0200

    fix(frontend): Add server error handling

--- frontend/src/features/url-shortener/services/url-shortener.service.ts
+++ frontend/src/features/url-shortener/services/url-shortener.service.ts
@@ -1,16 +1,32 @@
-import {config} from "../../../contexts/config";
 import { UrlShortenerFormValue } from "../form/url-shortener-form";
 
 export class UrlShortenerService {
-    private host = config.hosts.urlShortener;
+    constructor(private host: string) {}
 
-    shorten(formValue: UrlShortenerFormValue): Promise<{shortUrl: string}> {
-        return fetch(this.host + '/shorten', {
-            method: 'POST',
-            body: JSON.stringify(formValue),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }).then(res => res.json());
+    shorten(formValue: UrlShortenerFormValue): Promise<{ shortUrl: string }> {
+        return fetch(
+            this.host + '/shorten',
+            JSONPostRequestInit(formValue)
+        ).then(JSONResponseHandler);
     }
+}
+
+function JSONResponseHandler(res: Response) {
+    return res.json().then(data => {
+        if (res.ok) {
+            return data;
+        } else {
+            throw new Error(data && data.error || "Something went wrong. Please try again later");
+        }
+    });
+}
+
+function JSONPostRequestInit(formValue: UrlShortenerFormValue): RequestInit  {
+    return {
+        method: 'POST',
+        body: JSON.stringify(formValue),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
 }
